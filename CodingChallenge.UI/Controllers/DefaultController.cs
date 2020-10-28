e467fd964be8758efd1aa95dbfa095c9fbff641d
krishhari1984 <krishhari1984@users.noreply.github.com>
2020-10-28 19:35:58 -0400

    test

--- CodingChallenge.UI/Controllers/DefaultController.cs
+++ CodingChallenge.UI/Controllers/DefaultController.cs
@@ -8,7 +8,6 @@ namespace CodingChallenge.UI.Controllers
 {
     public class DefaultController : Controller
     {
-        //Test check-in
         public ILibraryService LibraryService { get; private set; }
 
         public DefaultController() { }
